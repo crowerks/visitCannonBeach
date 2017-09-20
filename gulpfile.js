f020c35728825bf5775e80b26c7b9e7156f8be01
Thomas Robinson <thomas@crowerks.com>
2017-09-19 17:23:17 -0700

    add autoprefixer, move index to src, add copy

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
+var autoprefixer = require('gulp-autoprefixer');
 
 var SOURCEPATHS = {
-
-  sassSource : 'src/scss/*.scss'
+  sassSource : 'src/scss/*.scss',
+  htmlSource : 'src/*.html'
 }
 var APPPATH = {
   root : 'app/',
@@ -13,12 +14,26 @@ var APPPATH = {
   js : 'app/js'
 }
 
+
+
+// makes sass work
 gulp.task('sass', function() {
     return gulp.src(SOURCEPATHS.sassSource)
+      .pipe(autoprefixer())
+      //1 autoprefixer adds -webkit-transition to the css
       .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+      //2 sass compiles .scss down to .css.
+      //options: expanded, nested, compressed, compact
       .pipe(gulp.dest(APPPATH.css));
+      //3 dest gives destination to write the css
 });
 
+gulp.task('copy', function() {
+  gulp.src(SOURCEPATHS.htmlSource)
+    .pipe(gulp.dest(APPPATH.root))
+});
+
+// serve creates the local server with browserSync running
 gulp.task('serve', ['sass'], function() {
   browserSync.init([APPPATH.css + '/*.css', APPPATH.root + '/*.html', APPPATH.js + '*.js'], {
     server : {
@@ -27,8 +42,11 @@ gulp.task('serve', ['sass'], function() {
   })
 });
 
-gulp.task('watch', ['serve', 'sass'], function() {
+//gulp watch makes changes once the sass has been updated
+gulp.task('watch', ['serve', 'sass', 'copy'], function() {
   gulp.watch([SOURCEPATHS.sassSource], ['sass']);
+  gulp.watch([SOURCEPATHS.htmlSource], ['copy'])
 });
 
+// this is the default gulp task
 gulp.task('default', ['watch']);
