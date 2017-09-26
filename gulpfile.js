6b70ea73ce5e2d69a06573741ccf36c34d635d15
Thomas Robinson <thomas@crowerks.com>
2017-09-26 13:43:18 -0700

    add merge css stream

--- gulpfile.js
+++ gulpfile.js
@@ -3,48 +3,65 @@ var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var autoprefixer = require('gulp-autoprefixer');
+var browserify =require('gulp-browserify');
 var clean = require('gulp-clean');
 var concat = require('gulp-concat');
+var merge = require('merge-stream');
 
+//Set ths sources for backend development
 var SOURCEPATHS = {
   sassSource : 'src/scss/*.scss',
   htmlSource : 'src/*.html',
   jsSource : 'src/js/**'
 }
+
+//Set the sources for the frontend display
 var APPPATH = {
   root : 'app/',
   css : 'app/css',
   js : 'app/js'
 }
 
+//this keeps html in src and app synced
 gulp.task('clean-html', function() {
   return gulp.src(APPPATH.root + '/*.html', {read: false, force: true})
     .pipe(clean());
 });
+
+//this keeps scripts in src and app synced
 gulp.task('clean-scripts', function() {
   return gulp.src(APPPATH.js + '/*.js', {read: false, force: true})
     .pipe(clean());
 });
 
-
 // makes sass work
 gulp.task('sass', function() {
-    return gulp.src(SOURCEPATHS.sassSource)
+    var bootstrapCSS = gulp.src('node_modules/bootstrap/dist/css/bootstrap.css');
+    var sassFiles;
+
+    sassFiles = gulp.src(SOURCEPATHS.sassSource)
       .pipe(autoprefixer())
       //1 autoprefixer adds -webkit-transition to the css
       .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
       //2 sass compiles .scss down to .css.
       //options: expanded, nested, compressed, compact
+
+      //this merges the css together. Order dictates the css output
+      return merge(bootstrapCSS, sassFiles)
+      .pipe(concat('app.css'))
       .pipe(gulp.dest(APPPATH.css));
       //3 dest gives destination to write the css
 });
 
+//compiles everything to main.js in app
 gulp.task('scripts', ['clean-scripts'], function() {
   gulp.src(SOURCEPATHS.jsSource)
   .pipe(concat('main.js'))
+  .pipe(browserify())
   .pipe(gulp.dest(APPPATH.js))
 });
 
+//creates html files in app
 gulp.task('copy', ['clean-html'], function() {
   gulp.src(SOURCEPATHS.htmlSource)
     .pipe(gulp.dest(APPPATH.root))
